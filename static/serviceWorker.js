7b525e527f1f91f15fea90680bdc289e98253cde
Robert Schumann <robert.schumann@unic.com>
2018-12-07 08:53:38 +0100

    extend serviceWorker

--- static/serviceWorker.js
+++ static/serviceWorker.js
@@ -1,6 +1,41 @@
 function install(event) {
-  
+    console.log('install')
+    event.waitUntil(
+        caches.open('static-content')
+        .then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/vendor.js',
+                '/app.js',
+            ])
+        })
+    )
 }
 
+function activate(event) {
+    console.log('activate')
+}
+
+function handleFetch(event) {
+    console.log('fetch')
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            if(response) {
+                return response
+            }
+
+            return fetch(event.request)
+        })
+        .catch(function () {
+            return caches.match('/index.html')
+        })
+    )
+}
 
 self.addEventListener('install', install)
+
+self.addEventListener('fetch', handleFetch)
+
+self.addEventListener('activate', activate)
